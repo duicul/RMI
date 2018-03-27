b6f2f48836919db8d52f24b8c676cc96ebad439f
laur <duicul@yahoo.com>
2018-03-27 11:35:05 +0300

    lampp

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rmiregistry &
-java -Djava.rmi.server.codebase=http://$1/~duicul/rmi/ -Djava.rmi.server.hostname=$1 -Djava.security.policy=server.policy ComputeEngine;
+java -Djava.rmi.server.codebase=http://$1/RMI/ -Djava.rmi.server.hostname=$1 -Djava.security.policy=server.policy ComputeEngine;
 
