b90d5d47fedd692280083c539ec91dc63ae417f0
laur <duicul@yahoo.com>
2018-04-04 19:50:37 +0300

    adj

--- Compute.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Compute extends Remote {
-    public int execute(int x) throws RemoteException;
-    public Data getData() throws RemoteException;
-}
